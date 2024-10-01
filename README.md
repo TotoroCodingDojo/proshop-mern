49481cde6ccb958e0efc30730ea6fed5f3d417b1
TotoroCodingDojo <physicist.artist@gmail.com>
2024-10-01 11:43:53 +0530

    till 2.5 done

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ecommerce site using MERN stack
+
+## Resources
+
+-   <https://stackoverflow.com/questions/69832748/error-error-a-route-is-only-ever-to-be-used-as-the-child-of-routes-element>
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,40 @@
+import { React } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar
+                bg="dark"
+                data-bs-theme="dark"
+                expand="lg"
+                className="bg-body-tertiary"
+                collapseOnSelect
+            >
+                <Container>
+                    <LinkContainer to="/">
+                        <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <LinkContainer to="/cart">
+                                <Nav.Link>
+                                    <i className="fas fa-shopping-cart"></i>Cart
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <Nav.Link>
+                                    <i className="fas fa-user"></i>Sign In
+                                </Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+};
+
+export default Header;
