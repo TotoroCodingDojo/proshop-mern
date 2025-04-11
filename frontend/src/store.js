bfd0d9f8a1e6066121201b10793d54dfa478f091
TotoroCodingDojo <physicist.artist@gmail.com>
2025-04-11 11:28:30 +0530

    6 done

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,12 +7,21 @@ import {
     productDetailReducer,
 } from "./reducers/productReducers";
 
+import { cartReducer } from "./reducers/cartReducers";
+
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailReducer,
+    cart: cartReducer,
 });
 
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+    ? JSON.parse(localStorage.getItem("cartItems"))
+    : [];
+
+const initialState = {
+    cart: { cartItems: cartItemsFromStorage },
+};
 
 const middleware = [thunk];
 
