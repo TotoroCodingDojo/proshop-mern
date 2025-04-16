eb5fe08969ef54be440b614f9f2d05dcf0289ef6
TotoroCodingDojo <physicist.artist@gmail.com>
2025-04-17 01:38:00 +0530

    8 done frontend authenciations

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,20 +7,36 @@ import {
     productDetailReducer,
 } from "./reducers/productReducers";
 
+import {
+    userLoginReducer,
+    userRegisterReducer,
+    userDetailsReducer,
+    userUpdateProfileReducer,
+} from "./reducers/userReducers";
+
 import { cartReducer } from "./reducers/cartReducers";
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
     ? JSON.parse(localStorage.getItem("cartItems"))
     : [];
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null;
+
 const initialState = {
     cart: { cartItems: cartItemsFromStorage },
+    userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
